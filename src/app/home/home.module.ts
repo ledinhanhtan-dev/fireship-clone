3a9c0f095becd486b5c54e0e59c2ef1b12df64b2
ledinhanhtan-dev <ledinhanhtan.dev@gmail.com>
2021-10-18 14:37:13 +0700

    feat: add home-navigation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeHeroComponent } from './components/home-hero/home-hero.component';
 import { SharedModule } from '../shared/shared.module';
-import { HomeTestimonialComponent } from './components/home-testimonial/home-testimonial.component';
-import { HomeCoursesComponent } from './components/home-courses/home-courses.component';
-import { HomeLessonsComponent } from './components/home-lessons/home-lessons.component';
+
+import * as index from './components';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [HomeComponent, HomeHeroComponent, HomeTestimonialComponent, HomeCoursesComponent, HomeLessonsComponent],
+  declarations: [HomeComponent, index.components],
   imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
 })
 export class HomeModule {}
