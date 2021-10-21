dc568de55f890428437471a31f6f9d48167ae0d9
ledinhanhtan-dev <ledinhanhtan.dev@gmail.com>
2021-10-21 15:27:57 +0700

    feat: add highligh.js

--- src/app/lessons/components/lesson-content/lesson-content.component.ts
+++ src/app/lessons/components/lesson-content/lesson-content.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightAutoResult } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-lesson-content',
   templateUrl: './lesson-content.component.html',
-  styleUrls: ['./lesson-content.component.scss']
+  styleUrls: ['./lesson-content.component.scss'],
 })
 export class LessonContentComponent implements OnInit {
+  code = `import { BsPlus, BsFillLightningFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
 
-  constructor() { }
+const SideBar = () => {
+  return (
+    <div className="...">
+      <SideBarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<BsPlus size="32" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcon icon={<FaPoo size="20" />} />
+    </div>
+  );
+};
 
-  ngOnInit(): void {
-  }
+const SideBarIcon = ({ icon }) => (
+  <div className="sidebar-icon group">
+    {icon}
+  </div>
+);`;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/lessons/lessons.module.ts
+++ src/app/lessons/lessons.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { LessonsRoutingModule } from './lessons-routing.module';
 import { LessonsComponent } from './lessons.component';
+import { HighlightModule } from 'ngx-highlightjs';
 import { EmbedPipe } from './pipes/embed.pipe';
 
 import * as index from './components';
 
 @NgModule({
   declarations: [LessonsComponent, index.components, EmbedPipe],
-  imports: [CommonModule, SharedModule, LessonsRoutingModule],
+  imports: [CommonModule, SharedModule, LessonsRoutingModule, HighlightModule],
 })
 export class LessonsModule {}
