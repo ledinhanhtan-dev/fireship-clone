aa52a8b94a17fa42475ad8b7377d9d2be551dc08
ledinhanhtan-dev <ledinhanhtan.dev@gmail.com>
2021-10-21 13:43:16 +0700

    feat: add app-file component

--- src/app/lessons/components/index.ts
+++ src/app/lessons/components/index.ts
@@ -4,12 +4,14 @@ import { LessonVideoComponent } from './lesson-video/lesson-video.component';
 import { LessonsPaginationComponent } from './lessons-pagination/lessons-pagination.component';
 import { LessonAuthorComponent } from './lesson-author/lesson-author.component';
 import { LessonQnaComponent } from './lesson-qna/lesson-qna.component';
+import { LessonContentComponent } from './lesson-content/lesson-content.component';
 
 export const components: any[] = [
   LessonComponent,
   LessonTocComponent,
   LessonVideoComponent,
   LessonAuthorComponent,
+  LessonContentComponent,
   LessonsPaginationComponent,
   LessonQnaComponent,
 ];
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -3,6 +3,7 @@ import { ButtonComponent } from './button/button.component';
 import { CardCourseComponent } from './card-course/card-course.component';
 import { CardLessonComponent } from './card-lesson/card-lesson.component';
 import { CardTweetComponent } from './card-tweet/card-tweet.component';
+import { FileComponent } from './file/file.component';
 import { IconComponent } from './icon/icon.component';
 import { LinkComponent } from './link/link.component';
 import { TagComponent } from './tag/tag.component';
@@ -16,6 +17,7 @@ export const components: any[] = [
   CardLessonComponent,
   CardTweetComponent,
   IconComponent,
+  FileComponent,
   TagComponent,
   VideoComponent,
 ];
